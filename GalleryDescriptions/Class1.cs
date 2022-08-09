9c7a1716fc4a17bc80daf61d1a6b38b16bb95151
mcrom <mcrom@LAPTOP-1OJNI6O5>
2022-08-08 22:11:37 -0400

    Add project files.

---
+++ GalleryDescriptions/Class1.cs
@@ -0,0 +1,310 @@
+﻿namespace GalleryDescriptions
+{
+    <PropertyGroup>
+    <TargetFramework>netstandard2.0</TargetFramework>
+    <LangVersion>8.0</LangVersion>
+    </PropertyGroup>
+    <ItemGroup>
+        <NONE REMOVE= "Gallery\alien_gallery.png" />
+        < NONE REMOVE= "Gallery\alien_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\alien_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\alien_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\alien_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\alien_ships_large.png" />
+        < NONE REMOVE = "Gallery\apple_fields_large.png" />
+        < NONE REMOVE = "Gallery\apple_gallery.png" />
+        < NONE REMOVE = "Gallery\apple_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\apple_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\apple_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\apple_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\apple_ship_large.png" />
+        < NONE REMOVE = "Gallery\apple_world_large.png" />
+        < NONE REMOVE = "Gallery\athlete_gallery.png" />
+        < NONE REMOVE = "Gallery\athlete_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\athlete_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\cheese_gallery.png" />
+        < NONE REMOVE = "Gallery\cheese_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\cheese_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\chimera_croc_large.png" />
+        < NONE REMOVE = "Gallery\chimera_gallery.png" />
+        < NONE REMOVE = "Gallery\chimera_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\chimera_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\chimera_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\chimera_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\clay_gallery.png" />
+        < NONE REMOVE = "Gallery\clay_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\clay_galleryThumb.png" />
+        < NONE REMOVE = "Gallery\clay_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\clay_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\clay_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\diver_gallery.png" />
+        < NONE REMOVE = "Gallery\diver_gallery_altpose.png.png" />
+        < NONE REMOVE = "Gallery\diver_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\diver_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\diver_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\donnie_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\duo_gallery.png" />
+        < NONE REMOVE = "Gallery\duo_galleryThumb.png" />
+        < NONE REMOVE = "Gallery\duo_kitchen_large.png" />
+        < NONE REMOVE = "Gallery\duo_mad_large.png" />
+        < NONE REMOVE = "Gallery\duo_picture_unlock.png.png" />
+        < NONE REMOVE = "Gallery\duo_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\gary_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\goth_gallery.png" />
+        < NONE REMOVE = "Gallery\goth_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\gromble_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\hero_gallery.png" />
+        < NONE REMOVE = "Gallery\hero_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\kite_earth_large.png" />
+        < NONE REMOVE = "Gallery\kite_gallery.png" />
+        < NONE REMOVE = "Gallery\kite_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\kite_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\kite_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\kite_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\kite_portals_large.png" />
+        < NONE REMOVE = "Gallery\kite_temple_large.png" />
+        < NONE REMOVE = "Gallery\life_show_large.png" />
+        < NONE REMOVE = "Gallery\mascot_blocks_large.png" />
+        < NONE REMOVE = "Gallery\mascot_gallery.png" />
+        < NONE REMOVE = "Gallery\mascot_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\mascot_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\mascot_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\mascotfight_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\mascotfight_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\moon_dome_large.png" />
+        < NONE REMOVE = "Gallery\moon_gallery.png" />
+        < NONE REMOVE = "Gallery\moon_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\moon_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\moon_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\moon_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\moon_roof_large.png" />
+        < NONE REMOVE = "Gallery\moon_sludge_large.png" />
+        < NONE REMOVE = "Gallery\narrator_gallery.png" />
+        < NONE REMOVE = "Gallery\narrator_jungle_large.png" />
+        < NONE REMOVE = "Gallery\narrator_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\neo_gallery.png" />
+        < NONE REMOVE = "Gallery\neo_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\neo_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\neo_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\neo_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\neo_transformer_large.png" />
+        < NONE REMOVE = "Gallery\orb_dream_large.png" />
+        < NONE REMOVE = "Gallery\orb_gallery.png" />
+        < NONE REMOVE = "Gallery\orb_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\orb_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\orb_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\orb_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\pizza_gallery.png" />
+        < NONE REMOVE = "Gallery\pizza_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\pizza_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\pizza_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\pizza_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\plasma_gallery.png" />
+        < NONE REMOVE = "Gallery\plasma_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\plasma_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\plasma_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\plasma_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\plasma_zone_large.png" />
+        < NONE REMOVE = "Gallery\rascal_cemetery_large.png" />
+        < NONE REMOVE = "Gallery\rascal_gallery.png" />
+        < NONE REMOVE = "Gallery\rascal_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\rascal_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\rascal_house_large.png" />
+        < NONE REMOVE = "Gallery\rascal_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\rascal_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\reporter_gallery.png" />
+        < NONE REMOVE = "Gallery\reporter_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\reporter_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\rival_bus_large.png" />
+        < NONE REMOVE = "Gallery\rival_gallery.png" />
+        < NONE REMOVE = "Gallery\rival_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\rival_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\rival_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\rival_picture_unlock_thumb.png" />
+        < NONE REMOVE = "Gallery\snake_dump_large.png" />
+        < NONE REMOVE = "Gallery\snake_gallery.png" />
+        < NONE REMOVE = "Gallery\snake_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\star_gallery.png" />
+        < NONE REMOVE = "Gallery\star_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\star_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\star_picture_unlock.png" />
+        < NONE REMOVE = "Gallery\stimpy_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\stimpy_galleryThumb_altpose.png" />
+        < NONE REMOVE = "Gallery\pie_abstract_large.png" />
+        < NONE REMOVE = "Gallery\pie_gallery.png" />
+        < NONE REMOVE = "Gallery\pie_gallery_altpose.png" />
+        < NONE REMOVE = "Gallery\pie_galleryThumb_altpose.png" />
+    </ItemGroup>
+
+    </ItemGroup>
+        <EmbeddedResource Include = "Gallery\alien_gallery.png" />
+        < EmbeddedResource Include= "Gallery\alien_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\alien_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\alien_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\alien_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\alien_ships_large.png" />
+        < EmbeddedResource Include = "Gallery\apple_fields_large.png" />
+        < EmbeddedResource Include = "Gallery\apple_gallery.png" />
+        < EmbeddedResource Include = "Gallery\apple_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\apple_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\apple_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\apple_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\apple_ship_large.png" />
+        < EmbeddedResource Include = "Gallery\apple_world_large.png" />
+        < EmbeddedResource Include = "Gallery\athlete_gallery.png" />
+        < EmbeddedResource Include = "Gallery\athlete_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\athlete_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\cheese_gallery.png" />
+        < EmbeddedResource Include = "Gallery\cheese_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\cheese_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\chimera_croc_large.png" />
+        < EmbeddedResource Include = "Gallery\chimera_gallery.png" />
+        < EmbeddedResource Include = "Gallery\chimera_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\chimera_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\chimera_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\chimera_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\clay_gallery.png" />
+        < EmbeddedResource Include = "Gallery\clay_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\clay_galleryThumb.png" />
+        < EmbeddedResource Include = "Gallery\clay_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\clay_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\clay_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\diver_gallery.png" />
+        < EmbeddedResource Include = "Gallery\diver_gallery_altpose.png.png" />
+        < EmbeddedResource Include = "Gallery\diver_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\diver_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\diver_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\donnie_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\duo_gallery.png" />
+        < EmbeddedResource Include = "Gallery\duo_galleryThumb.png" />
+        < EmbeddedResource Include = "Gallery\duo_kitchen_large.png" />
+        < EmbeddedResource Include = "Gallery\duo_mad_large.png" />
+        < EmbeddedResource Include = "Gallery\duo_picture_unlock.png.png" />
+        < EmbeddedResource Include = "Gallery\duo_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\gary_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\goth_gallery.png" />
+        < EmbeddedResource Include = "Gallery\goth_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\gromble_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\hero_gallery.png" />
+        < EmbeddedResource Include = "Gallery\hero_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\kite_earth_large.png" />
+        < EmbeddedResource Include = "Gallery\kite_gallery.png" />
+        < EmbeddedResource Include = "Gallery\kite_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\kite_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\kite_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\kite_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\kite_portals_large.png" />
+        < EmbeddedResource Include = "Gallery\kite_temple_large.png" />
+        < EmbeddedResource Include = "Gallery\life_show_large.png" />
+        < EmbeddedResource Include = "Gallery\mascot_blocks_large.png" />
+        < EmbeddedResource Include = "Gallery\mascot_gallery.png" />
+        < EmbeddedResource Include = "Gallery\mascot_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\mascot_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\mascot_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\mascotfight_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\mascotfight_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\moon_dome_large.png" />
+        < EmbeddedResource Include = "Gallery\moon_gallery.png" />
+        < EmbeddedResource Include = "Gallery\moon_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\moon_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\moon_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\moon_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\moon_roof_large.png" />
+        < EmbeddedResource Include = "Gallery\moon_sludge_large.png" />
+        < EmbeddedResource Include = "Gallery\narrator_gallery.png" />
+        < EmbeddedResource Include = "Gallery\narrator_jungle_large.png" />
+        < EmbeddedResource Include = "Gallery\narrator_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\neo_gallery.png" />
+        < EmbeddedResource Include = "Gallery\neo_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\neo_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\neo_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\neo_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\neo_transformer_large.png" />
+        < EmbeddedResource Include = "Gallery\orb_dream_large.png" />
+        < EmbeddedResource Include = "Gallery\orb_gallery.png" />
+        < EmbeddedResource Include = "Gallery\orb_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\orb_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\orb_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\orb_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\pizza_gallery.png" />
+        < EmbeddedResource Include = "Gallery\pizza_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\pizza_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\pizza_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\pizza_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\plasma_gallery.png" />
+        < EmbeddedResource Include = "Gallery\plasma_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\plasma_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\plasma_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\plasma_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\plasma_zone_large.png" />
+        < EmbeddedResource Include = "Gallery\rascal_cemetery_large.png" />
+        < EmbeddedResource Include = "Gallery\rascal_gallery.png" />
+        < EmbeddedResource Include = "Gallery\rascal_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\rascal_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\rascal_house_large.png" />
+        < EmbeddedResource Include = "Gallery\rascal_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\rascal_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\reporter_gallery.png" />
+        < EmbeddedResource Include = "Gallery\reporter_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\reporter_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\rival_bus_large.png" />
+        < EmbeddedResource Include = "Gallery\rival_gallery.png" />
+        < EmbeddedResource Include = "Gallery\rival_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\rival_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\rival_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\rival_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\snake_dump_large.png" />
+        < EmbeddedResource Include = "Gallery\snake_gallery.png" />
+        < EmbeddedResource Include = "Gallery\snake_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\star_gallery.png" />
+        < EmbeddedResource Include = "Gallery\star_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\star_picture_unlock.png" />
+        < EmbeddedResource Include = "Gallery\star_picture_unlock_thumb.png" />
+        < EmbeddedResource Include = "Gallery\stimpy_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\stimpy_galleryThumb_altpose.png" />
+        < EmbeddedResource Include = "Gallery\pie_abstract_large.png" />
+        < EmbeddedResource Include = "Gallery\pie_gallery.png" />
+        < EmbeddedResource Include = "Gallery\pie_gallery_altpose.png" />
+        < EmbeddedResource Include = "Gallery\pie_galleryThumb_altpose.png" />
+     </ItemGroup>
+        
+     <ItemGroup>
+    <Reference Include = "0Harmony" >
+      < HintPath > Refs\0Harmony.dll</HintPath>
+    </Reference>
+    <Reference Include = "Assembly-CSharp" >
+      < HintPath > Refs\Assembly-CSharp.dll</HintPath>
+    </Reference>
+    <Reference Include = "BepInEx" >
+      < HintPath > Refs\BepInEx.dll</HintPath>
+    </Reference>
+    <Reference Include = "UnityEngine" >
+      < HintPath > Refs\UnityEngine.dll </ HintPath >
+    </ Reference >
+    < Reference Include="UnityEngine.CoreModule">
+      <HintPath>Refs\UnityEngine.CoreModule.dll</HintPath>
+    </Reference>
+    <Reference Include = "UnityEngine.ImageConversionModule" >
+      < HintPath > Refs\UnityEngine.ImageConversionModule.dll </ HintPath >
+    </ Reference >
+    < Reference Include="UnityEngine.UI">
+      <HintPath>Refs\UnityEngine.UI.dll</HintPath>
+    </Reference>
+  </ItemGroup>
+
+  <ItemGroup>
+    <Folder Include = "Patches\" />
+  </ ItemGroup >
+
+  < Target Name="PostBuild" AfterTargets="PostBuildEvent">
+    <Exec Command = "if $(ConfigurationName) == Debug (&#xD;&#xA;copy /Y &quot;$(TargetPath)&quot; &quot;C:\Program Files\Steam\steamapps\common\Nickelodeon All-Star Brawl\BepInEx\plugins&quot;&#xD;&#xA;)" />
+  </ Target >
+
+</ Project >
+
+
+    public class Class1
+    {
+
+    }
+}
