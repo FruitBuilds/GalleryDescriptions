294ce1e753d3e73cfee3d028b15ece386189d7c1
FruitBuilds <96547355+FruitBuilds@users.noreply.github.com>
2022-08-20 15:08:25 -0400

    Add files via upload

---
+++ GalleryDescriptions/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GalleryDescriptions.Managers;
+using HarmonyLib;
+using BepInEx.Configuration;
+
+namespace GalleryDescriptions
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static Plugin Instance;
+        internal static ConfigEntry<bool> 
+
+        void Awake()
+        {
+            if (Instance)
+            {
+                DestroyImmediate(this);
+                return;
+            }
+            Instance = this;
+
+           
+
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+        }
+
+        #region logging
+        internal static void LogDebug(string message) => Instance.Log(message, LogLevel.Debug);
+        internal static void LogInfo(string message) => Instance.Log(message, LogLevel.Info);
+        internal static void LogWarning(string message) => Instance.Log(message, LogLevel.Warning);
+        internal static void LogError(string message) => Instance.Log(message, LogLevel.Error);
+        private void Log(string message, LogLevel logLevel) => Logger.Log(logLevel, message);
+        #endregion
+    }
+}
