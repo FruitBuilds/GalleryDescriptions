53f0d3505b6df8555f6ae45816988b89ecd34864
FruitBuilds <96547355+FruitBuilds@users.noreply.github.com>
2022-08-20 15:13:24 -0400

    Add files via upload

---
+++ GalleryDescriptions/PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace GalleryDescriptions
+{
+    class PluginInfo
+    {
+        public const string PLUGIN_GUID = "com.fruitbuilds.nasb.gallerydescriptions";
+        public const string PLUGIN_NAME = "Gallery Descriptions";
+        public const string PLUGIN_VERSION = "1.4.1";
+    }
+}
