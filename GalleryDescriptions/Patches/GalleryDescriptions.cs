09461dda78abe9b4b0c92bfd292f208a166dd499
FruitBuilds <96547355+FruitBuilds@users.noreply.github.com>
2022-09-25 14:14:41 -0400

    Update GalleryDescriptions.cs

--- GalleryDescriptions/Patches/GalleryDescriptions.cs
+++ GalleryDescriptions/Patches/GalleryDescriptions.cs
@@ -33,7 +33,7 @@ static class GalleryScreen_OnPressedGallerySlot
         if (galleryManager.TryGetGallerySprite(gallerySlot.galleryPictureData.ID, out Sprite sprite))
         {
              Plugin.LogDebug($"Replacing gallery images for {agent.GameUniqueIdentifier}");
-                    for (int i = 0; i < ___stocks.Length; i++)
+                    for (int i = 0; i < ___gallery.Length; i++)
                         ___gallery[i].sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(texture.width / 2f, texture.height / 2f));
             // if true then you can replace the image
 
