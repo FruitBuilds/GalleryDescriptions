8de62d404ffeaf0e236d2a3f6771a1d84a5adc7e
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2022-11-09 18:40:10 -0800

    Fixed things up.

--- GalleryDescriptions/Patches/GalleryDescriptions.cs
+++ GalleryDescriptions/Patches/GalleryDescriptions.cs
@@ -1,43 +1,27 @@
-﻿using GalleryDescriptions.Utilities;
+﻿using GalleryDescriptions.Managers;
 using HarmonyLib;
 using Nick;
-using System.Collections;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
+using System.Linq;
+using System.Reflection;
 
 namespace GalleryDescriptions.Patches
 {
-    [HarmonyPatch(typeof(GalleryScreen), OnPressedGallerySlot)]
-static class GalleryScreen_OnPressedGallerySlot
-{
-    static void Postfix(GallerySlot gallerySlot, SetSlotPortrait ___galleryPicture)
+    [HarmonyPatch(typeof(GalleryScreen), nameof(GalleryScreen.OnPressedGallerySlot))]
+    static class GalleryScreen_OnPressedGallerySlot
     {
-        if (___galleryslot == -1) return;
+        static BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+        static FieldInfo seekTex = typeof(SetSlotPortrait).GetField("seekTex", bindingFlags);
 
-            if (!isCoroutineRunning)
-            {
-                gi = Resources.FindObjectsOfTypeAll<GameInstance>().FirstOrDefault(x => x.AgentCount > 0);
-                SharedCoroutineStarter.StartCoroutine(NullGameInstance());
-            }
-
-            if (gi.GetAgentFromgalleryslot(___galleryslot, out var agent))
-            {
-                if (!agent) return;
-        // need some method to look up an id string and return a sprite
-        // you also need some kind of manager to handle loading the new gallery images from disk or from embedded resources
-        // see: https://github.com/DeadlyKitten/CustomStockIcons/blob/master/CustomStockIcons/Managers/IconManager.cs
-        // to generate a Sprite from a texture you can use the following code, where `texture` is the texture you want to convert
-        
-        if (galleryManager.TryGetGallerySprite(gallerySlot.galleryPictureData.ID, out Sprite sprite))
+        static void Postfix(GallerySlot gallerySlot, SetSlotPortrait ___galleryPicture)
         {
-             Plugin.LogDebug($"Replacing gallery images for {agent.GameUniqueIdentifier}");
-                    for (int i = 0; i < ___gallery.Length; i++)
-                        ___gallery[i].sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(texture.width / 2f, texture.height / 2f));
-            // if true then you can replace the image
-
-            // not 100% sure this will work, if not you might need to use reflection or something to change some things on this object
-            ___galleryPicture.SetSprite(sprite);
+            var id = gallerySlot.galleryPictureData.PicturePath.Split('/').Last();
+            if (GalleryManager.TryGetGalleryImage(id, out Texture2D texture))
+            {
+                ___galleryPicture.rawImage.texture = texture;
+                seekTex.SetValue(___galleryPicture, false);
+                ___galleryPicture.rawImage.enabled = true;
+            }
         }
     }
 }
